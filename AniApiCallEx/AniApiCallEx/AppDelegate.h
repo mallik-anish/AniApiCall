e297d6f15d4c9dd94295ec46a9e3c6fec838c6f7
anish mallik <mallik.anish@gmail.com>
2019-03-15 13:44:36 +0530

    Api Call

---
+++ AniApiCallEx/AniApiCallEx/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AniApiCallEx
+//
+//  Created by Anish Kumar Mallik on 15/03/19.
+//  Copyright © 2019 Anish Mallik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AniApiCallEx/AniApiCallEx/ViewController.m
@@ -0,0 +1,48 @@
+//
+//  ViewController.m
+//  AniApiCallEx
+//
+//  Created by Anish Kumar Mallik on 15/03/19.
+//  Copyright © 2019 Anish Mallik. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "AniApiCall/AniApiMethod.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // post method
+    
+    NSMutableDictionary *dic =[[NSMutableDictionary alloc] initWithObjectsAndKeys:@"morpheus",@"name",@"leader",@"job", nil];
+    [[AniApiMethod sharedManager] callPOSTJSONMethod:[NSURL URLWithString:@"https://reqres.in/api/users"] parameter:dic completion:^(NSData *data, NSError *error) {
+        NSError *jsonErro;
+        if(data!=nil)
+        {
+            NSMutableDictionary *jsonAry=[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&jsonErro];
+            NSLog(@"%@",jsonAry);
+            
+        }
+    }];
+    
+    // rest method
+    
+    [[AniApiMethod sharedManager] callGETJSONMethodApi:[NSURL URLWithString:@"https://reqres.in/api/users?page=2"] completion:^(NSData *data, NSError *error) {
+        NSError *jsonErro;
+        if(data!=nil)
+        {
+            NSMutableDictionary *jsonAry=[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&jsonErro];
+            NSLog(@"%@",jsonAry);
+            
+        }
+    }];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
